c77b308e2800e77cf2138bb0d83c8a0d2ab09366
LuigiVanin <luisfvaninmartins@gmail.com>
2025-03-27 23:35:42 -0300

    Feat ✨: adding gradient blob to gallery card component

--- src/components/Gallery/GalleryCard.tsx
+++ src/components/Gallery/GalleryCard.tsx
@@ -2,6 +2,7 @@
 
 import { useEffect, useRef } from "react";
 import { useMouse } from "react-use"
+// import {motion} from "motion/react"
 
 // reference: https://codesandbox.io/p/sandbox/framer-motion-mouse-position-2b4sd?file=%2Fsrc%2FApp.js%3A90%2C1-91%2C1
 
@@ -19,24 +20,30 @@ type GalleryCardProps = {
 
 const GalleryCard = (props: GalleryCardProps) => {
   const cardRef = useRef(null as any);
-  const { docX, docY, elW, elH } = useMouse(cardRef);
+  const { docX, docY, elW, elH, posX, posY } = useMouse(cardRef);
 
   useEffect(() => {
     console.log(cardRef)
   }, [])
 
   return (
-    <div ref={cardRef} className="">
-      <div className="h-96 w-96 absolute blur-3xl" style={{
-        background: "linear-gradient(135deg, rgb(59, 196, 242), rgb(122, 105, 249), rgb(242, 99, 120), rgb(245, 131, 63))",
-        maskImage: "radial-gradient(190px, white, transparent)",
-        left: docX - 192,
-        top: docY - 192,
-      }} />
-      <span>{docX}</span>
-      /
-      <span>{docY}</span>
-      / {elW}
+    <div ref={cardRef} className="group/card hover:scale-[1.01] transition-transform duration-200 w-96 h-72  rounded-2xl  relative overflow-hidden shadow-card p-[1px] ">
+
+      <div
+        className="z-0 h-80 w-80 absolute transition-transform duration-500 group-hover/card:scale-[1.65] opacity-80 bg-[linear-gradient(135deg,_rgb(59,196,242),_rgb(122,105,249),_rgb(242,99,120),_rgb(245,131,63))] [mask-image:radial-gradient(190px,_white,_transparent)]"
+        style={{
+          left: docX - 192 - posX,
+          top: docY - 192 - posY,
+        }} />
+
+      <div className="z-10 h-full w-full bg-background/60 p-4 relative rounded-2xl dark:bg-calm-200/60 [backdrop-filter:blur(150px)]">
+
+        <div className="w-20 h-20 bg-background/40"></div>
+        <span>{docX - 192 - posX}</span>
+        <br />
+        <span>{docY - 192 - posY}</span>
+      </div>
+
     </div>
   )
 }
