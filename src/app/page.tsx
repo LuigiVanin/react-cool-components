aeb962b284af24010b69ec912ed76a98aa5cc348
LuigiVanin <luisfvaninmartins@gmail.com>
2025-04-01 00:12:15 -0300

    Feat ✨: finishing the gallery Card component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 
 import PriceCard from "@/components/Gallery/PriceCard";
 import GalleryCard from "@/components/Gallery/GalleryCard";
+import { TvIcon } from "lucide-react";
 
 export default function Home() {
   const items = [
@@ -21,8 +22,16 @@ export default function Home() {
   return (
     <main className="w-full min-h-full flex gap-5 items-center justify-center p-5 bg-background">
       {/* <PriceCard value={29} items={items} /> */}
-      <GalleryCard />
-      <GalleryCard />
+      <GalleryCard className="flex flex-col items-center justify-start gap-2 w-96 h-72 cursor-pointer">
+        <div className="w-full py-7 flex items-center justify-center bg-calm-300/20 rounded-2xl">
+          <TvIcon className="w-16 h-16 text-primary" />
+        </div>
+        <div className="flex flex-col gap-2 flex-1 py-3">
+          <h1 className="text-3xl font-bold">Welcome to our Gallery</h1>
+          <p className="text-sm text-muted-foreground">Explore our collection of stunning artworks.</p>
+        </div>
+      </GalleryCard>
+      <GalleryCard className="w-96 h-72" />
 
     </main>
   );
--- src/components/Gallery/GalleryCard.tsx
+++ src/components/Gallery/GalleryCard.tsx
@@ -1,46 +1,47 @@
 "use client";
 
-import { useEffect, useRef } from "react";
-import { useMouse } from "react-use"
-import { motion } from "motion/react"
+import { forwardRef, useImperativeHandle, useRef } from "react";
+import { useMouse } from "react-use";
+import { motion } from "motion/react";
+import { ClassValue } from "clsx";
+import { cn } from "@/lib/cn";
 
 // reference: https://codesandbox.io/p/sandbox/framer-motion-mouse-position-2b4sd?file=%2Fsrc%2FApp.js%3A90%2C1-91%2C1
 
 type GalleryCardProps = {
-
+  wrapperClassName?: ClassValue
+  className?: ClassValue
+  children?: React.ReactNode
 }
 
-
-const GalleryCard = (props: GalleryCardProps) => {
-
+const GalleryCard = forwardRef(({ children, wrapperClassName, className }: GalleryCardProps, ref) => {
   const cardRef = useRef(null as any);
-  const { docX, docY, posX, posY } = useMouse(cardRef);
 
+  useImperativeHandle(ref, () => cardRef.current);
+
+  const { docX, docY, posX, posY, elW, elH } = useMouse(cardRef);
 
   return (
-    <div ref={cardRef} className="group/card hover:scale-[1.02] transition-transform duration-200 w-96 h-72  rounded-2xl  relative overflow-hidden shadow-card p-[1.5px] ">
+    <div ref={cardRef} className={cn("group/card hover:scale-[1.02] transition-transform duration-300   rounded-2xl  relative overflow-hidden shadow-card p-[1.5px]", wrapperClassName)}>
 
       <motion.div
-        className="h-96 w-96 absolute opacity-80 blur-2xl z-0
-        bg-[linear-gradient(135deg,_rgb(59,196,242),_rgb(122,105,249),_rgb(242,99,120),_rgb(245,131,63))] [mask-image:radial-gradient(190px,_white,_transparent)]
+        className="h-96 w-96 absolute opacity-80 blur-xl z-0
+        bg-[linear-gradient(135deg,_rgb(59,196,242),_rgb(122,105,249),_rgb(242,99,120),_rgb(245,131,63))] [mask-image:radial-gradient(190px,_white,_transparent)] group-hover/card:scale-200 transition-transform duration-200
         "
         style={{
-          left: docX - (384 / 2) - posX,
-          top: docY - (384 / 2) - posY,
+          left: docX - (elW / 2) - posX,
+          top: docY - (elH / 2) - posY,
         }}
-        initial={{ scale: 1 }}
-        whileHover={{ scale: 1.85 }}
-        transition={{ duration: 0.2, type: "spring" }}
       />
 
 
-      <div className="z-10 h-full w-full bg-background/80 p-4 relative rounded-2xl dark:bg-calm-200/75 ">
-
+      <div className={cn("z-10 h-full w-full bg-background/85 p-4 relative rounded-2xl dark:bg-calm-200/80 [backdrop-filter:_blur(50px)]", className)}>
+        {children}
       </div>
 
     </div>
   )
-}
+})
 
 export default GalleryCard;
 
