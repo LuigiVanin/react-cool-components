e4b2c79458f054957ccb798ddf7a5d0d2799b032
LuigiVanin <luisfvaninmartins@gmail.com>
2025-05-04 20:00:50 -0300

    Feat ✨: finishing the swipe action item

--- src/app/gallery/swipe-action/page.tsx
+++ src/app/gallery/swipe-action/page.tsx
@@ -1,12 +1,28 @@
 "use client"
 import SwipeAction from "@/components/Gallery/swipe-action"
-import { EditIcon, Trash2Icon } from "lucide-react"
+import { EditIcon, LoaderCircleIcon, Trash2Icon, TvIcon } from "lucide-react"
+import { useState } from "react"
 
 const SwipeActionPage = () => {
+  const [fakeLoading, setFakeLoading] = useState(false)
+
+  const customAction = (payload: { close: () => void, x: number }) => {
+    if (fakeLoading) return
+    setFakeLoading(true)
+
+    setTimeout(() => {
+      payload.close()
+      setFakeLoading(false)
+      payload.close()
+      alert(`Action completed with x: ${payload.x}`)
+    }, 2000)
+  }
+
   return (
-    <main className="w-full min-h-full flex gap-5 items-center justify-center p-5">
+    <main className="w-full min-h-full flex gap-2 items-center justify-center flex-col p-5">
       <SwipeAction
-        className="w-80"
+        snapLimit={200}
+        className="w-80 cursor-pointer"
         leftAction={({ close }) =>
           <div className="flex items-center justify-end w-full h-full overflow-x-hidden relative">
             <button
@@ -27,15 +43,66 @@ const SwipeActionPage = () => {
         rightAction={({ close, x }) =>
           <div className="flex items-center justify-end w-full h-full overflow-x-hidden relative">
             <button
-              className="cursor-pointer min-w-12 w-full bg-sky-400 text-white h-full absolute right-0 z-20 flex-center"
-              onClick={close}
+              className="cursor-pointer min-w-12 w-full bg-sky-400 text-white h-full absolute right-0 z-20 flex-center flex-col"
+              onClick={() => customAction({ close, x })}
             >
-              {Math.abs(x).toFixed(1)}
+              {
+                fakeLoading ?
+                  <LoaderCircleIcon className="animate-spin" /> :
+                  <>
+                    <p>
+                      Action
+                    </p>
+                    <p>
+                      <strong>
+                        {Math.abs(x).toFixed(1)}px
+                      </strong>
+                    </p>
+                  </>
+              }
             </button>
 
           </div>
         }
-      />
+      >
+
+        <div className="flex items-start justify-start py-4 px-6 gap-3 bg-background dark:bg-calm-100">
+
+          <TvIcon className="" />
+          <div className="flex flex-col items-start justify-start">
+            <h1 className="font-semibold">Action Card</h1>
+            <p className="text-sm text-calm-500">
+              Swipe right or left aby kind of action
+            </p>
+          </div>
+        </div>
+
+      </SwipeAction>
+
+      <SwipeAction
+        snapLimit={100}
+        className="w-80 cursor-pointer"
+        leftAction={({ close }) =>
+          <div className="flex items-center justify-end w-full h-full overflow-x-hidden relative">
+            <button
+              className="cursor-pointer min-w-12 w-full bg-rose-500 text-white h-full absolute left-0 z-20 flex-center"
+              onClick={close}
+            >
+              <Trash2Icon />
+            </button>
+          </div>
+        }
+      >
+        <div className="bg-background dark:bg-calm-100 flex items-start justify-start py-4 px-6 gap-3">
+          <div className="flex flex-col items-start justify-start">
+            <h1 className="font-semibold">Action Card</h1>
+            <p className="text-sm text-calm-500">
+              Swipe right or left aby kind of action
+            </p>
+          </div>
+        </div>
+
+      </SwipeAction>
     </main>
   )
 }
--- src/components/Gallery/swipe-action/index.tsx
+++ src/components/Gallery/swipe-action/index.tsx
@@ -1,74 +1,74 @@
 "use client"
 import { motion, useMotionValue, animate } from "motion/react";
-import { TvIcon } from "lucide-react";
-import { ReactNode, useEffect, useRef, useState } from "react";
+import { forwardRef, PropsWithChildren, ReactNode, useEffect, useMemo, useState } from "react";
 import { cn } from "@/lib/cn";
 
 const SNAP_LIMIT = 130;
 
 type SwipeStatus = "left" | "right" | "none";
 
-type SwipeActionProps = {
+type SwipeActionProps = PropsWithChildren<{
   className?: string;
   rightAction?: ReactNode | ((payload: { status: SwipeStatus, close: () => void, x: number }) => ReactNode);
   leftAction?: ReactNode | ((payload: { status: SwipeStatus, close: () => void, x: number }) => ReactNode);
-}
+  snapLimit?: number;
+}>
 
-const SwipeAction = ({ leftAction, rightAction, className }: SwipeActionProps) => {
-  const swiperAcitionContainerRef = useRef<HTMLDivElement>(null);
-  const x = useMotionValue(0);
-  const [xState, setXState] = useState(0);
-  const [swipeStatus, setSwipeStatus] = useState<SwipeStatus>("none");
+const SwipeAction = forwardRef<HTMLDivElement, SwipeActionProps>(
+  ({ children, leftAction, rightAction, className, snapLimit = SNAP_LIMIT }, ref) => {
+    const x = useMotionValue(0);
+    const [xState, setXState] = useState(0);
+    const [swipeStatus, setSwipeStatus] = useState<SwipeStatus>("none");
 
-  useEffect(() => {
-    const unsubscribe = x.on("change", () => {
-      setXState(x.get());
+    useEffect(() => {
+      const unsubscribe = x.on("change", () => {
+        setXState(x.get());
 
-      if (x.get() < -(SNAP_LIMIT / 3)) {
-        setSwipeStatus("left");
-      } else if (x.get() > SNAP_LIMIT / 3) {
-        setSwipeStatus("right");
+        if (x.get() < -(snapLimit / 2) && rightAction) {
+          setSwipeStatus("left");
+        } else if (x.get() > snapLimit / 2 && leftAction) {
+          setSwipeStatus("right");
+        } else {
+          setSwipeStatus("none");
+        }
+      });
+
+      return () => unsubscribe();
+    }, [x, leftAction, rightAction, snapLimit]);
+
+    const handleDragEnd = () => {
+      if (swipeStatus === "left") {
+        // Snap to -100px
+        animate(x, -snapLimit, { ease: [0.4, 0, 0.2, 1] });
+      } else if (swipeStatus === "right") {
+        // Snap to 100px (if needed)
+        animate(x, snapLimit, { ease: [0.4, 0, 0.2, 1] });
       } else {
-        setSwipeStatus("none");
+        // Snap back to 0
+        animate(x, 0);
       }
-    });
+    };
 
-    return () => unsubscribe();
-  }, [x]);
-
-  const handleDragEnd = () => {
-    if (swipeStatus === "left") {
-      // Snap to -100px
-      animate(x, -SNAP_LIMIT, { ease: [0.4, 0, 0.2, 1] });
-    } else if (swipeStatus === "right") {
-      // Snap to 100px (if needed)
-      animate(x, SNAP_LIMIT, { ease: [0.4, 0, 0.2, 1] });
-    } else {
-      // Snap back to 0
-      animate(x, 0);
+    const closeDraggableItem = () => {
+      if (swipeStatus !== 'none') {
+        animate(x, 0, { ease: [0.4, 0, 0.2, 1] });
+      }
     }
-  };
 
-  const closeDraggableItem = () => {
-    if (swipeStatus !== 'none') {
-      animate(x, 0, { ease: [0.4, 0, 0.2, 1] });
-    }
-  }
+    const dragConstraints = useMemo(() => {
+      return {
+        left: rightAction ? -snapLimit : 0,
+        right: leftAction ? snapLimit : 0,
+      }
+    }, [snapLimit, leftAction, rightAction]);
 
-  return (
-    <div className="flex flex-col items-center justify-center w-full h-full">
-      <p>
-        {/* {xState} */}
-      </p>
-      <p>
-        {swipeStatus}
-      </p>
+    return (
       <motion.div
-        ref={swiperAcitionContainerRef}
-        className={cn("swipe-action__container group overflow-hidden relative shadow-sm group-hover:shadow-md transition-shadow duration-200 rounded-lg border border-calm-200 flex-center", className)}
+        ref={ref}
+        className={cn("swipe-action__container group overflow-hidden relative shadow-sm group-hover:shadow-md transition-shadow duration-200 rounded-lg border border-calm-200 flex-center bg-background dark:bg-calm-100", className)}
       >
         <motion.div
-          className="swipe-action--left h-full flex items-start justify-betweencursor-pointer ease-in-out absolute left-0 z-10 rounded-l-lg "
+          className="swipe-action--left h-full flex items-start justify-between ease-in-out absolute left-0 z-10 rounded-l-lg "
           style={{
             width: Math.max(xState, 0)
           }}>
@@ -80,7 +80,7 @@ const SwipeAction = ({ leftAction, rightAction, className }: SwipeActionProps) =
         </motion.div>
 
         <motion.div
-          className="swipe-action--right h-full  flex items-start justify-betweencursor-pointer ease-in-out absolute right-0 z-10 rounded-r-lg"
+          className="swipe-action--right h-full  flex items-start justify-between ease-in-out absolute right-0 z-10 rounded-r-lg"
           style={{
             width: Math.max(xState * -1, 0)
           }}
@@ -95,33 +95,25 @@ const SwipeAction = ({ leftAction, rightAction, className }: SwipeActionProps) =
         </motion.div>
 
         <motion.div
-          className="swipe-action  bg-background/80 dark:bg-calm-100 flex items-start justify-start py-4 px-6 gap-3 cursor-pointer ease-in-out z-30 w-full"
-          whileDrag={{ cursor: "grabbing !important" }}
+          className="swipe-action ease-in-out z-30 w-full "
+          whileDrag={{ cursor: "grabbing" }}
           drag="x"
           style={{
             x,
           }}
           dragMomentum={false}
-          dragElastic={0.2}
-          dragConstraints={{
-            left: -SNAP_LIMIT,
-            right: SNAP_LIMIT,
-          }}
-
+          dragElastic={0.15}
+          dragConstraints={dragConstraints}
           onDragEnd={handleDragEnd} // Handle snapping on drag end
           onClick={closeDraggableItem} // Handle click to reset
+          dragSnapToOrigin={false}
         >
-          <TvIcon className="" />
-          <div className="flex flex-col items-start justify-start">
-            <h1 className="font-semibold">Action Card {xState.toFixed(2)}</h1>
-            <p className="text-sm text-calm-500">
-              Swipe right or left aby kind of action
-            </p>
-          </div>
+          {children}
         </motion.div>
       </motion.div>
-    </div>
-  );
-};
+    );
+  });
+
+SwipeAction.displayName = "SwipeAction";
 
 export default SwipeAction;
