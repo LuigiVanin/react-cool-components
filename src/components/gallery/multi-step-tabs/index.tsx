d4d82817fd91652be5b845ddfdd043010e172b18
LuigiVanin <luisfvaninmartins@gmail.com>
2025-05-20 22:30:01 -0300

    Feat ✨: creating animation for tab transition

--- src/components/gallery/multi-step-tabs/index.tsx
+++ src/components/gallery/multi-step-tabs/index.tsx
@@ -2,7 +2,7 @@
 import { cn } from "@/lib/cn";
 import { CheckIcon } from "lucide-react";
 import { AnimatePresence, motion } from "motion/react";
-import { useMemo, useState } from "react";
+import { useMemo, useRef, useState } from "react";
 
 const MultiStepTab = () => {
   const [currentTab, setCurrentTab] = useState(0);
@@ -11,16 +11,26 @@ const MultiStepTab = () => {
     return Array.from({ length: 3 }, (_, index) => ({ text: String(index + 1), index }));
   }, [])
 
-  console.log("tabs: ", tabs);
+  const tabsContent = useMemo(() => {
+    return Array.from({ length: 3 }, (_, index) => ({ text: `Content ${index + 1}`, index }));
+  }, [])
+
+  const contentRefs = useRef<(HTMLDivElement | null)[]>([]);
+
+  const [contentHeight, setContentHeight] = useState<number>(0);
 
   return (
-    <div className="flex flex-col w-96 h-48 items-center justify-start bg-calm-100 rounded-xl border border-calm-200/65 shadow shadow-calm-400/25">
-      <header className="w-full h-16 px-3 gap-4 flex items-center justify-around bg-calm-200/50 rounded-t-xl">
+    <div className="flex flex-col w-96 min-h-48 items-center justify-start bg-calm-100 rounded-xl border border-calm-200/65 shadow-xs dark:shadow-calm-400/25">
+      <header className="w-full px-3 py-2 gap-4 flex items-center justify-around bg-calm-200/50 rounded-t-xl">
         {
           tabs.map((tab) => (
-            <button
+            <motion.button
               key={tab.index}
-              className={cn(`h-11 w-11 cursor-pointer rounded-full border border-calm-100 bg-calm-200/50 font-semibold text-calm-600 hover:text-calm-900 transition-all duration-200 flex-center relative`, currentTab > tab.index && 'bg-calm-300')}
+              className={cn(
+                `h-11 w-11 cursor-pointer rounded-full border border-calm-100 bg-calm-200/50 font-semibold text-calm-600 hover:text-calm-900 transition-all duration-200 flex-center relative`,
+                currentTab > tab.index && 'bg-calm-300',
+                currentTab === tab.index && 'border-2 border-calm-700 bg-calm-200',
+              )}
               onClick={() => setCurrentTab(tab.index)}
             >
               <AnimatePresence mode="wait">
@@ -49,10 +59,53 @@ const MultiStepTab = () => {
                 )}
               </AnimatePresence>
 
-            </button>
+            </motion.button>
           ))
         }
       </header>
+
+      <motion.main
+        className="w-full overflow-x-hidden flex flex-row overflow-y-hidden"
+        animate={{ height: contentHeight }}
+        transition={{ duration: 0.22, type: "spring", stiffness: 85 }}
+        style={{ height: !contentHeight ? "auto" : contentHeight }}
+      >
+        <AnimatePresence mode="wait">
+          {tabsContent.map((tab, index) =>
+            index === currentTab &&
+            (
+              <motion.div
+                key={tab.index}
+                className={cn(`w-full h-auto flex-start text-calm-900 font-semibold text-xl`)}
+                initial={{ opacity: 0, x: "60%" }}
+                animate={{ opacity: 1, x: "0%" }}
+                exit={{ opacity: 0, x: "-50%" }}
+                transition={{ duration: 0.25 }}
+              >
+                <motion.div
+                  className="h-auto w-full"
+                  ref={(el) => {
+                    if (el?.offsetHeight !== contentHeight)
+                      setContentHeight(el?.offsetHeight || 0);
+                    contentRefs.current[index] = el;
+                  }}
+                >
+                  <motion.div
+                    className={cn(
+                      "p-4",
+                      currentTab === 0 && 'h-40',
+                      currentTab === 1 && 'h-64',
+                      currentTab === 2 && 'h-96'
+                    )}
+
+                  >
+                    {tab.text}
+                  </motion.div>
+                </motion.div>
+              </motion.div>
+            ))}
+        </AnimatePresence>
+      </motion.main>
     </div>
   );
 }
